c97ff13ee69bb2b67d54fcc75cd10feb2671d60c
jasonpatel <jasonpatel@users.noreply.github.com>
2020-12-01 23:17:54 +0000

    Update carspeed.py
    min speed 28 mph

--- carspeed.py
+++ carspeed.py
@@ -119,7 +119,7 @@ def store_traffic_data():
 # define some constants
 L2R_DISTANCE = 50.68  #<---- enter your distance-to-road value for cars going left to right here
 R2L_DISTANCE = 39.86  #<---- enter your distance-to-road value for cars going left to right here
-MIN_SPEED_IMAGE = 35  #<---- enter the minimum speed for saving images
+MIN_SPEED_IMAGE = 28  #<---- enter the minimum speed for saving images
 SAVE_CSV = True  #<---- record the results in .csv format in carspeed_(date).csv
 MIN_SPEED_SAVE = 10  #<---- enter the minimum speed for publishing to MQTT broker and saving to CSV
 MAX_SPEED_SAVE = 80  #<---- enter the maximum speed for publishing to MQTT broker and saving to CSV
